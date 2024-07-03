9161f7cab3d1f9493660928d317dc49763624569
Jaimicolss2 <jbouffanaispaz@gmail.com>
2024-07-03 16:48:21 -0400

    Se optimiza el tiempo de ejecucion de q2_time

--- src/q2_time.py
+++ src/q2_time.py
@@ -2,15 +2,11 @@ import json
 import re
 from collections import defaultdict
 from typing import List, Tuple
-from emoji import UNICODE_EMOJI
 
-def extract_emojis(text: str) -> List[str]:
-    return [char for char in text if char in UNICODE_EMOJI['en']]
-
-def q2_time(file_path: str) -> List[Tuple[str, int]]:
+def q2_memory(file_path: str) -> List[Tuple[str, int]]:
     emoji_counts = defaultdict(int)
-    emoji_pattern = re.compile("[\U00010000-\U0010ffff]", flags=re.UNICODE)
-    
+    emoji_pattern = re.compile(r'[\U0001F300-\U0001F64F]|[\U0001F680-\U0001F6FF]|[\U0001F700-\U0001F77F]|[\U0001F780-\U0001F7FF]|[\U0001F800-\U0001F8FF]', flags=re.UNICODE)
+
     with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
             try:
@@ -25,12 +21,12 @@ def q2_time(file_path: str) -> List[Tuple[str, int]]:
     top_10_emojis = sorted(emoji_counts.items(), key=lambda x: x[1], reverse=True)[:10]
     return top_10_emojis
 
-# Ejemplo de uso
-file_path = "C:/Users/mauri/OneDrive/Escritorio/Challenge Latam/farmers-protest-tweets-2021-2-4.json"
-try:
-    top_10_emojis = q2_time(file_path)
-    print(top_10_emojis)
-except FileNotFoundError:
-    print(f"File not found: {file_path}. Please check the file path and try again.")
-except json.JSONDecodeError as e:
-    print(f"Error parsing JSON file: {e}")
+if __name__ == "__main__":
+    file_path = "C:/Users/mauri/OneDrive/Escritorio/Challenge Latam/farmers-protest-tweets-2021-2-4.json"
+    try:
+        top_10_emojis = q2_memory(file_path)
+        print(top_10_emojis)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}. Please check the file path and try again.")
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON file: {e}")
